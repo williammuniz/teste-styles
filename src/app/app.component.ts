3f2b024430877415ffb2eb1edd6bf1daaba8ffbf
William Muniz <william.muniz@webpublico.com.br>
2024-11-29 18:19:03 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'teste-ngstyle';
+  fontSize: number = 15;
+  textColor: 'white' | 'orange' = 'white';
+  buttonText: 'Branco' | 'Laranja' = 'Laranja'
+
+  aumentarFontSize() {
+    this.fontSize += 1;
+  }
+
+  changeColor() {
+    if (this.textColor === 'white') {
+      this.textColor = 'orange';
+      this.buttonText = 'Branco';
+    }else{
+      this.textColor = 'white';
+      this.buttonText = 'Laranja';
+    }
+  }
 }
